23494446e565f2ea6c97a1b49d166fd64d1f1009
amirahmadadibi <amirahmadadibi@gmail.com>
2019-07-26 14:44:49 +0430

    create a base class

---
+++ src/Blub.kt
@@ -0,0 +1,15 @@
+class Blub {
+    var color: String = "data"
+
+    private var isOn: Boolean = false
+
+    fun switchOn() {
+        isOn = true
+    }
+
+    fun switchOff() {
+        isOn = false
+    }
+
+
+}
